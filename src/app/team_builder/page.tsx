21fe813fb66b60603dedd54fe08cc39641c009a5
Damian Petroff <damian.petroff@outlook.com>
2024-02-10 04:28:24 +0100

    test: rename lib

--- src/app/team_builder/page.tsx
+++ src/app/team_builder/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Champion, getChampions, getVersions } from "../../lib/@ddragon";
+import { Champion, getChampions, getVersions } from "../../lib/ddragon";
 
 export default async function Home() {
   const versions: Array<string> = await getVersions();
